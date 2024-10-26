255a9aee5fd00a2f2c101fb8755c4b6c3a629483
IlyasSam <shambazovilyas@gmail.com>
2024-10-26 13:19:34 +0300

    react

---
+++ src/components/HeaderComponent/HeaderComponent.jsx
@@ -0,0 +1,27 @@
+import style from './Header.module.css'
+import logo from '/src/assets/avtotorgLogo 1.svg'
+
+export function Header() {
+    return (
+        <>
+            <div className="center">
+                <header className='container'>
+                    <nav className={style.nav}>
+                        <a href="#!">О вас</a>
+                        <a href="#!">Каталог</a>
+                        <a href="#!">Где нас найти</a>
+                    </nav>
+                    <img src={logo} alt="s" className={style.logo}>
+
+                    </img>
+                    
+                    <div className={style.header_btns}>
+                        <button className={style.left}>Регистрация</button>
+                        <button className={style.right}>Вход</button>
+                    </div>
+                </header>
+            </div>
+
+        </>
+    )
+}
